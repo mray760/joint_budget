80fed53c2c6fefdbff1f0967952134869c25be23
mray760 <94726783+mray760@users.noreply.github.com>
2025-11-23 10:13:09 -0700

    Create categories.py

---
+++ categories.py
@@ -0,0 +1,21 @@
+categories = [
+    'Amys Allowance',
+    'Automotive',
+    'Gas',
+    'Groceries',
+    'Home',
+    'Insurance',
+    'Leisure',
+    'Mortgage',
+    'Pet Food',
+    'Subscriptions',
+    'Utilities',
+    'Rent',
+    'Direct Deposit - Matt',
+    'Direct Deposit - Amy',
+    'Venmo Income',
+    'Venmo Expense',
+    'Zelle Income',
+    'Zelle Expense'
+
+]
