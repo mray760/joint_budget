02ccc0a27c7985bd736d53419594c20d73893b7e
mray760 <94726783+mray760@users.noreply.github.com>
2025-11-27 10:08:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from load_transactions import load_sofi, load_chase
 from expense_summary import build_expense_summary
 from income_summary import build_income_summary
 from to_excel import write_excel
-from functions import combine_joint_chase
+from functions import combine_joint_chase, normalize_output
 from Categories import categories
+from summary import run_summary
 
 to_excel = True
 
@@ -20,27 +21,30 @@ selected_periods = all_periods[all_periods.index(start): all_periods.index(end)+
 print("Selected periods:", selected_periods)
 
 
-chase_filename = 'chase_test'
-sofi_filename = 'sofi_test'
+chase_filename = 'chase'
+sofi_filename = 'sofi'
 
 
 def run_pipeline(chase_filename,sofi_filename):
     df_sofi = load_sofi(sofi_filename, periods = selected_periods,categories=categories)
     df_chase = load_chase(chase_filename, periods=selected_periods,categories=categories)
-    summary_expense, alerts = build_expense_summary(df_sofi=df_sofi,df_chase=df_chase)
-    summary_income = build_income_summary(df_sofi,df_chase)
+    outflows, alerts = build_expense_summary(df_sofi=df_sofi,df_chase=df_chase)
+    inflows = build_income_summary(df_sofi,df_chase)
     combined_df = combine_joint_chase(df_chase=df_chase,df_joint=df_sofi)
+    norm_outflows = normalize_output(outflows)
+    summary = run_summary(df_outflows=outflows,df_inflows=inflows)
+
     if to_excel == True:
-        write_excel(date=to_date,expense_df=summary_expense,income_df=summary_income,combined_df=combined_df)
+        write_excel(date=to_date,outflow_df=norm_outflows,inflow_df=inflows,combined_df=combined_df,summary_df=summary)
     else:
         print("to_excel is set to False")
-    return summary_expense, alerts , summary_income
+
 
 
 
 if __name__ == "__main__":
 
-    summary_expense, alerts, summary_income = run_pipeline(chase_filename,sofi_filename)
+   run_pipeline(chase_filename,sofi_filename)
 
 
 
