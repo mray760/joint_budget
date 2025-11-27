8c6c974001902cb9e2c2cf0d7335817a0c9b4058
mray760 <94726783+mray760@users.noreply.github.com>
2025-11-27 10:08:42 -0700

    Update categories.py

--- categories.py
+++ categories.py
@@ -16,6 +16,39 @@ categories = [
     'Venmo Income',
     'Venmo Expense',
     'Zelle Income',
-    'Zelle Expense'
+    'Zelle Expense',
+    'Investments'
 
 ]
+
+
+
+income_cat =[
+    'Rent',
+    'Direct Deposit - Matt',
+    'Direct Deposit - Amy',
+    'Vencmo Income',
+    'Zelle Income',
+    'Other Income'
+]
+
+
+expense_cat =[
+'   Amys Allowance',
+    'Automotive',
+    'Gas',
+    'Groceries',
+    'Home',
+    'Insurance',
+    'Leisure',
+    'Mortgage',
+    'Pet Food',
+    'Subscriptions',
+    'Utilities',
+    'Venmo Expense',
+    'Zelle Expense',
+    'Other Expense'
+]
+
+
+investment_cat =['Investments']
