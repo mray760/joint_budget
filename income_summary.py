8e7d4c1d8f786aa116ec929aca3a6819ce902c97
mray760 <94726783+mray760@users.noreply.github.com>
2026-01-03 12:19:27 -0700

    Update income_summary.py

--- income_summary.py
+++ income_summary.py
@@ -1,28 +1,14 @@
 import pandas as pd
 
 
-def build_income_summary(df_sofi, df_chase):
-    cc = df_chase
-    checking = df_sofi
+def build_income_summary(combined_transactions):
+    df = combined_transactions
 
-    # Credit card: expenses are negative; ignore payments & returns
-    cc_income = cc[cc["Amount"] > 0]
+    df = df[df["Amount"]>0]
 
-    # Checking: expenses are negative; ignore deposits
-    checking_income = checking[checking["Amount"] > 0]
-
-    # Combine
-    combined = pd.concat([
-        cc_income[["Category", "Amount"]],
-        checking_income[["Category", "Amount"]],
-    ])
-    summary = combined.groupby("Category")["Amount"].sum().abs()
+    summary = df.groupby("Category")["Amount"].sum().abs()
 
     summary_income = summary.to_frame().reset_index()
     summary_income.columns = ["Category", "Amount"]
-
-
-
-
 
     return summary_income
