a7cc361f268d57cbe9f4b50ada4c55f3f97a7e2e
mray760 <94726783+mray760@users.noreply.github.com>
2026-01-03 12:18:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
 import pandas as pd
-from load_transactions import load_sofi, load_chase
-from expense_summary import build_expense_summary
-from income_summary import build_income_summary
-from to_excel import write_excel
-from functions import combine_joint_chase, normalize_output
-from Categories import categories
-from summary import run_summary
-
-to_excel = True
-
-from_date = '11/2025'
-to_date = '11/2025'
 
 
 
-all_periods = pd.date_range("2025-01-01", "2025-12-01", freq="MS").strftime("%m/%Y").tolist()
-start, end = from_date, to_date
-selected_periods = all_periods[all_periods.index(start): all_periods.index(end)+1]
+from data_intake.load_transactions import load_sofi, load_chase
+from reporting.income_summary import build_income_summary
+from export.to_excel import write_excel
+from normalization.functions import combine_joint_chase, run_periods_norm, create_categories
+from normalization.post_di_norm import chase_norm,sofi_norm
+from reporting.summary import run_summary
+from reporting.expense_summary import build_expense_summary
+from scratch import build_expense_summary
 
-print("Selected periods:", selected_periods)
 
+to_excel = False
+
+from_date = '12/2025'
+to_date = '12/2025'
 
 chase_filename = 'chase'
 sofi_filename = 'sofi'
 
 
+
 def run_pipeline(chase_filename,sofi_filename):
-    df_sofi = load_sofi(sofi_filename, periods = selected_periods,categories=categories)
-    df_chase = load_chase(chase_filename, periods=selected_periods,categories=categories)
-    outflows, alerts = build_expense_summary(df_sofi=df_sofi,df_chase=df_chase)
-    inflows = build_income_summary(df_sofi,df_chase)
-    combined_df = combine_joint_chase(df_chase=df_chase,df_joint=df_sofi)
-    norm_outflows = normalize_output(outflows)
+    selected_periods = run_periods_norm(to_date=to_date,from_date=from_date)
+    all_categories,expense_categories,income_categories,investment_categories = create_categories()
+
+    ## Data Intake
+    sofi_raw = load_sofi(sofi_filename, periods = selected_periods,categories=all_categories)
+    chase_raw = load_chase(chase_filename, periods=selected_periods,categories=all_categories)
+
+    ##Normalization
+    df_sofi_norm = sofi_norm(sofi_raw,categories=all_categories)
+    df_chase_norm = chase_norm(chase_raw,categories=all_categories)
+    combined_transactions = combine_joint_chase(df_chase=df_chase_norm,df_joint=df_sofi_norm)
+
+    ##Reporting
+    outflows = build_expense_summary(combined_transactions=combined_transactions,expense_cat=expense_categories,investment_cat=investment_categories)
+    inflows = build_income_summary(combined_transactions=combined_transactions)
     summary = run_summary(df_outflows=outflows,df_inflows=inflows)
 
     if to_excel == True:
-        write_excel(date=to_date,outflow_df=norm_outflows,inflow_df=inflows,combined_df=combined_df,summary_df=summary)
+        write_excel(date=to_date,outflow_df=outflows,inflow_df=inflows,combined_df=combined_transactions,summary_df=summary)
     else:
         print("to_excel is set to False")
 
